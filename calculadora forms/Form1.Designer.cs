463120f55dadcfeb0ac0673326bce1955c91e6c2
mrGois <ezequielgois@yahoo.com.br>
2025-02-28 17:04:41 -0300

    adicionado strings, if e else

--- calculadora forms/Form1.Designer.cs
+++ calculadora forms/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(587, 450);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
--- calculadora forms/Form1.cs
+++ calculadora forms/Form1.cs
@@ -7,9 +7,28 @@ namespace calculadora_forms
             InitializeComponent();
         }
 
+        private const int V = 0;
+        private System.Windows.Forms.TextBox txtScreen;
+
+        // variaveis
+        string operador = "";
+        double num1 = 0;
+        double num2 = 0;
         private void Form1_Load(object sender, EventArgs e)
         {
+            txtScreen.Text = V.ToString();
+            num1 = 0;
+            num2 = 0;
+            operador = V.ToString();
+
 
         }
+        private void btnBack_Click(object sender, EventArgs e)
+          {
+            if (txtScreen.TextLength == 1)
+                txtScreen.Text = "0";
+            else
+                txtScreen.Text = txtScreen.Text.Substring(0, txtScreen.Text.Length - 1);
+        }    
     }
 }
