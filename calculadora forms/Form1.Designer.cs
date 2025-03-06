d038879ff4824ae5e16b7a5238701412effb356b
mrGois <ezequielgois@yahoo.com.br>
2025-03-06 17:33:50 -0300

    alterações na calculadora

--- calculadora forms/Form1.Designer.cs
+++ calculadora forms/Form1.Designer.cs
@@ -28,19 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.ForeColor = SystemColors.Window;
+            button1.Location = new Point(12, 274);
+            button1.Name = "button1";
+            button1.Size = new Size(66, 50);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(282, 86);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaptionText;
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.ForeColor = SystemColors.Window;
+            button2.Location = new Point(84, 274);
+            button2.Name = "button2";
+            button2.Size = new Size(70, 50);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaptionText;
+            button3.Font = new Font("Segoe UI", 15F);
+            button3.ForeColor = SystemColors.Window;
+            button3.Location = new Point(232, 330);
+            button3.Name = "button3";
+            button3.Size = new Size(62, 50);
+            button3.TabIndex = 4;
+            button3.Text = "=";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaptionText;
+            button4.Font = new Font("Segoe UI", 15F);
+            button4.ForeColor = SystemColors.Window;
+            button4.Location = new Point(158, 274);
+            button4.Name = "button4";
+            button4.Size = new Size(68, 50);
+            button4.TabIndex = 3;
+            button4.Text = "3";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaptionText;
+            button5.Font = new Font("Segoe UI", 15F);
+            button5.ForeColor = SystemColors.Window;
+            button5.Location = new Point(232, 218);
+            button5.Name = "button5";
+            button5.Size = new Size(62, 50);
+            button5.TabIndex = 8;
+            button5.Text = "-";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaptionText;
+            button6.Font = new Font("Segoe UI", 15F);
+            button6.ForeColor = SystemColors.Window;
+            button6.Location = new Point(158, 162);
+            button6.Name = "button6";
+            button6.Size = new Size(68, 50);
+            button6.TabIndex = 7;
+            button6.Text = "9";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ActiveCaptionText;
+            button7.Font = new Font("Segoe UI", 15F);
+            button7.ForeColor = SystemColors.Window;
+            button7.Location = new Point(84, 162);
+            button7.Name = "button7";
+            button7.Size = new Size(70, 50);
+            button7.TabIndex = 6;
+            button7.Text = "8";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveCaptionText;
+            button8.Font = new Font("Segoe UI", 15F);
+            button8.ForeColor = SystemColors.Window;
+            button8.Location = new Point(12, 162);
+            button8.Name = "button8";
+            button8.Size = new Size(66, 50);
+            button8.TabIndex = 5;
+            button8.Text = "7";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveCaptionText;
+            button9.Font = new Font("Segoe UI", 15F);
+            button9.ForeColor = SystemColors.Window;
+            button9.Location = new Point(12, 218);
+            button9.Name = "button9";
+            button9.Size = new Size(66, 50);
+            button9.TabIndex = 5;
+            button9.Text = "4";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaptionText;
+            button10.Font = new Font("Segoe UI", 15F);
+            button10.ForeColor = SystemColors.Window;
+            button10.Location = new Point(84, 218);
+            button10.Name = "button10";
+            button10.Size = new Size(70, 50);
+            button10.TabIndex = 6;
+            button10.Text = "5";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.ActiveCaptionText;
+            button11.Font = new Font("Segoe UI", 15F);
+            button11.ForeColor = SystemColors.Window;
+            button11.Location = new Point(158, 218);
+            button11.Name = "button11";
+            button11.Size = new Size(68, 50);
+            button11.TabIndex = 7;
+            button11.Text = "6";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = SystemColors.ActiveCaptionText;
+            button12.Font = new Font("Segoe UI", 15F);
+            button12.ForeColor = SystemColors.Window;
+            button12.Location = new Point(232, 274);
+            button12.Name = "button12";
+            button12.Size = new Size(62, 50);
+            button12.TabIndex = 8;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = SystemColors.ActiveCaptionText;
+            button13.Font = new Font("Segoe UI", 15F);
+            button13.ForeColor = SystemColors.Window;
+            button13.Location = new Point(232, 162);
+            button13.Name = "button13";
+            button13.Size = new Size(62, 50);
+            button13.TabIndex = 12;
+            button13.Text = "X";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.ActiveCaptionText;
+            button14.Font = new Font("Segoe UI", 15F);
+            button14.ForeColor = SystemColors.Window;
+            button14.Location = new Point(158, 330);
+            button14.Name = "button14";
+            button14.Size = new Size(68, 50);
+            button14.TabIndex = 11;
+            button14.Text = "Delet";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.ActiveCaptionText;
+            button15.Font = new Font("Segoe UI", 15F);
+            button15.ForeColor = SystemColors.Window;
+            button15.Location = new Point(84, 330);
+            button15.Name = "button15";
+            button15.Size = new Size(70, 50);
+            button15.TabIndex = 10;
+            button15.Text = ",";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.ActiveCaptionText;
+            button16.Font = new Font("Segoe UI", 15F);
+            button16.ForeColor = SystemColors.Window;
+            button16.Location = new Point(12, 330);
+            button16.Name = "button16";
+            button16.Size = new Size(66, 50);
+            button16.TabIndex = 9;
+            button16.Text = "0";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.ActiveCaptionText;
+            button17.Font = new Font("Segoe UI", 15F);
+            button17.ForeColor = SystemColors.Window;
+            button17.Location = new Point(232, 106);
+            button17.Name = "button17";
+            button17.Size = new Size(62, 50);
+            button17.TabIndex = 13;
+            button17.Text = "clear";
+            button17.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(587, 450);
+            BackColor = SystemColors.InactiveCaptionText;
+            ClientSize = new Size(308, 402);
+            Controls.Add(button17);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button12);
+            Controls.Add(button5);
+            Controls.Add(button11);
+            Controls.Add(button6);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
     }
 }
--- calculadora forms/Form1.cs
+++ calculadora forms/Form1.cs
@@ -1,39 +1,75 @@
-namespace calculadora_forms
+nusing System;
+using System.Windows.Forms;
+
+namespace Calculadora_Forms
 {
     public partial class Form1 : Form
     {
+        double valor1 = 0, valor2 = 0;
+        string operacao = "";
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private const int V = 0;
-        private System.Windows.Forms.TextBox txtScreen;
-
-        // variaveis
-        string operador = "";
-        double num1 = 0;
-        double num2 = 0;
         private void Form1_Load(object sender, EventArgs e)
         {
-            txtScreen.Text = V.ToString();
-            num1 = 0;
-            num2 = 0;
-            operador = V.ToString();
-
-
+            txtScreen.Text = "0";
+            valor1 = 0;
+            valor2 = 0;
+            operacao = "";
         }
-        private void btnBack_Click(object sender, EventArgs e)
-          {
-            if (txtScreen.TextLength == 1)
-                txtScreen.Text = "0";
-            else
-                txtScreen.Text = txtScreen.Text.Substring(0, txtScreen.Text.Length - 1);
+
+        private void btnNumero_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (txtScreen.Text == "0")
+                txtScreen.Text = "";
+            txtScreen.Text += btn.Text;
         }
+
+        private void btnOperacao_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            valor1 = double.Parse(txtScreen.Text);
+            operacao = btn.Text;
+            txtScreen.Clear();
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            valor2 = double.Parse(txtScreen.Text);
+            switch (operacao)
+            {
+                case "+":
+                    txtScreen.Text = (valor1 + valor2).ToString();
+                    break;
+                case "-":
+                    txtScreen.Text = (valor1 - valor2).ToString();
+                    break;
+                case "*":
+                    txtScreen.Text = (valor1 * valor2).ToString();
+                    break;
+                case "/":
+                    txtScreen.Text = (valor1 / valor2).ToString();
+                    break;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtScreen.Clear();
+            valor1 = valor2 = 0;
+            operacao = "";
+        }
+
         private void btnBack_Click(object sender, EventArgs e)
         {
-            if (txtScreen.Text == "0") txtScreen.Text = "";
-            txtScreen.Text = txtScreen.Text + "1";
+            if (txtScreen.Text.Length > 0)
+                txtScreen.Text = txtScreen.Text.Substring(0, txtScreen.Text.Length - 1);
+            if (txtScreen.Text == "") ;
+                txtScreen.Text = "0";
         }
     }
 }
